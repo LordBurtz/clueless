2be5650b1ea8935e9f29bb86c53afc369c771edf
fridolin.karger <fridolin.karger@gmail.com>
2024-11-23 01:56:29 +0100

    e

--- src/db_manager.rs
+++ src/db_manager.rs
@@ -1,3 +1,4 @@
+use crate::GenericError;
 use crate::json_models::*;
 
 struct DBManager {
@@ -5,8 +6,8 @@ struct DBManager {
 }
 
 impl DBManager {
-    fn insert_offers(self, offers: PostRequestBodyModel) -> () {
-        return ();
+    fn insert_offers(self, offers: PostRequestBodyModel) -> Result<(), GenericError> {
+        return Ok(());
     }
 
     fn query_for(self, request_offer: RequestOffer) -> Option<GetReponseBodyModel> {
