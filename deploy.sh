59da90bdf0b7146beafc4487da936b40007f4ba7
challenger <challenger@tetet.de>
2024-11-26 18:25:29 +0000

    Add info about ulimit to deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,6 @@ RUSTFLAGS="-C target-feature=+aes,+sse2 -C target-cpu=native" cargo build -r
 #sudo nice -n -19 ./target/release/clueless
 #
 
+echo "Running with ulimit $(ulimit -n)"
 sudo ./target/release/clueless
 
